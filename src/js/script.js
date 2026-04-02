4378ad06c30608b05d4638ce9a3bdfcdcd9290fd
Leonardo Porto <leonardopsodre@gmail.com>
2026-04-02 16:50:53 -0300

    realizando o codigo de java script, e aperfeiçoando o index

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+// Coletando informações do usuário
+let nome = prompt("Digite o nome do vinho:");
+let tipo = prompt("Digite o tipo do vinho (Tinto, Branco, Rosé):");
+let safra = prompt("Digite a safra (ano):");
+let quantidade = prompt("Digite a quantidade em estoque:");
+
+alert("Cadastro realizado! Veja os detalhes no console.");
+
+console.log(`Nome: ${nome}`);
+console.log(`Tipo: ${tipo}`);
+console.log(`Safra: ${safra}`);
+console.log(`Quantidade em estoque: ${quantidade}`);
+
